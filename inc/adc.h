dcd0199941f249a6e0e2d307f39bb57797c07e9d
IgnaCleri <clerijuanignacio@gmail.com>
2026-05-23 16:46:30 -0300

    feat(adc): add ADC configuration and ISR logic

---
+++ inc/adc.h
@@ -0,0 +1,12 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+#include "LPC17xx.h"
+
+/**
+ * @brief Configura el ADC en modo burst para el canal 0,
+ * y habilita sus interrupciones.
+ */
+void ADC_Config(void);
+
+#endif /* ADC_H_ */
